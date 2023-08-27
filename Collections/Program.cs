79a832146fc10ef0b830fc2d958b1e7f4de64931
arbabmalik46 <arbabmalik46@gmail.com>
2023-08-27 14:18:11 +0500

    Added Collection Project and ArrayList

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,16 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace Collections
 {
+    #region Quiz Class
+    //class Quiz
+    //{
+    //    private int _indexnumber;
+
+    //    public int IndexNumber
+    //    {
+    //        get { return _indexnumber; }
+    //        set { _indexnumber = value; }
+    //    }
+
+    //    private string _question;
+
+    //    public string Question
+    //    {
+    //        get { return _question; }
+    //        set { _question = value; }
+    //    }
+    //}
+    #endregion
+        class E
+    {
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            
+
+            #region Collection Example Commented
+            //var c = new List<Quiz>();
+            //Quiz v = new Quiz();
+            //v.IndexNumber = 1;
+            //v.Question = "What is Your name?";
+            //c.Add(v);
+            //foreach (var item in c)
+            //{
+            //    Console.WriteLine(item.Question);
+            //    Console.WriteLine(item.IndexNumber);
+            //}
+            #endregion
+
+            #region Array Example
+            //int[] arr = new int[5];
+            //arr[0]= 1;
+            //arr[1]= 2;
+            //arr[2]= 3;
+            //arr[3]= 4;
+            //arr[4]= 5;
+            //Array.Resize(ref arr, 6);
+            //arr[5]= 6;
+            //Console.WriteLine(arr[5]);
+            #endregion
+
         }
     }
 }
---
+++ NonGenericCollections/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ArrayList
+            //ArrayList arrayList = new ArrayList();
+            //arrayList.Add(10); //To add the element
+            //arrayList.Add("hello world");
+            //arrayList.Add('c');
+            //arrayList.Add(2.34);
+            //E e = new E();
+            //arrayList.Add(e);
+            //Console.WriteLine(arrayList.Capacity);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //Console.WriteLine(arrayList.Capacity);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //arrayList.Add(2.34);
+            //Console.WriteLine(arrayList.Capacity);
+            //foreach (var item in arrayList)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //arrayList.Insert(1, 34); // to insert data at index -> input: index, value
+            //arrayList.Remove(2); // to delete a value in arrayList -> input: value
+            //arrayList.RemoveAt(3); // to delete a value at index in arratlist -> input: index
+
+            #endregion
+
+            #region HashTable
+
+            #endregion
+        }
+    }
+}
