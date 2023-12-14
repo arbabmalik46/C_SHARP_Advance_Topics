12335eeaa8499405ad85c4626d0219aaa90744d6
arbabmalik46 <arbabmalik46@gmail.com>
2023-12-14 23:36:44 +0500

    Added a new Project for File Handling Section

---
+++ FileHandling/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Runtime.InteropServices;
+
+namespace FileHandling
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+    
+}
